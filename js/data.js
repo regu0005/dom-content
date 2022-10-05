8321832611299fe25d88623139bff14a0e0cf3ec
Gustavo Reguerin <gustavoreguerin@Gustavos-MacBook-Pro.local>
2022-10-05 13:47:42 -0400

    all the starter files

---
+++ js/data.js
@@ -0,0 +1,10 @@
+let info = [
+    { tag: 'h3', content: 'Your Name' },
+    {
+      tag: 'p',
+      content: 'Born in City, Country.',
+      children: [{ tag: 'img', content: 'Your Name', src: './img/me.jpg' }],
+    },
+    { tag: 'p', content: 'Something about pets that you may have had.' },
+    { tag: 'p', content: 'Something about your siblings.' },
+  ];
